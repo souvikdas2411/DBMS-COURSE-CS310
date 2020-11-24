6b4a4ecdb90d8c64cb57d0e6a43746179ce3a8d1
souvikdas2411 <57383567+souvikdas2411@users.noreply.github.com>
2020-11-24 19:57:28 +0530

    Delete 5.sql

--- CarInsurance/5.sql
+++
@@ -1,9 +0,0 @@
-use mydb;
-SELECT 
-    t1.*
-FROM
-    t3_VEHICLE AS t1,
-    t3_PREMIUM_PAYMENT AS t2
-WHERE
-    t2.t3_Premium_Payment_Amount > t1.t3_Vehicle_Number
-        AND t1.t3_Cust_Id = t2.t3_Cust_Id; 
